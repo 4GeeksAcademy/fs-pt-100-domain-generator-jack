6f06ae92f8d0863b9e3179e30e48aa28fb85d5a1
Jacksson Sánchez <info@creativoz.es>
2025-03-14 11:46:26 +0000

    domain list

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+/* window.onload = function() {
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  const domain = ['.com', '.es', '.io', '.app']
+
+const arrD = ["pronoun", "adj", "noun"]
+  console.log(arrD);
+
+  pronoun.forEach(el => {
+    adj.forEach(val => {
+      noun.forEach(item => {
+        domain.forEach(dom => { 
+        console.log(`${el}  ${val} ${item} ${dom}`)
+      })
+    })
+  })
+});
+}
+*/
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  const domain = ['.com', '.es', '.io', '.app'];
+  const domainList = document.getElementById('domainList');
+
+  pronoun.forEach(el => {
+      adj.forEach(val => {
+          noun.forEach(item => {
+              domain.forEach(dom => {
+                  let domainName = `${el}${val}${item}${dom}`;
+                  let li = document.createElement('li');
+                  li.textContent = domainName;
+                  domainList.appendChild(li);
+              });
+          });
+      });
+  });
 };
+
